b7030d3580bf0aee6af8743c41524625d070b7d0
queentouchtechnology <sathish@queentouchtech.in>
2025-08-26 15:57:12 +0530

    classpath

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -6,7 +6,6 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.1.0")
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
